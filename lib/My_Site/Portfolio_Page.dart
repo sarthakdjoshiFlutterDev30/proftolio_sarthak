3da28b706608210eaf95284454b38a00f325262a
Sarthak Joshi <141836239+sarthakdjoshi@users.noreply.github.com>
2024-07-11 16:12:11 +0530

    init commit

--- lib/My_Site/Portfolio_Page.dart
+++ lib/My_Site/Portfolio_Page.dart
@@ -97,14 +97,13 @@ class _Protfolio_PageState extends State<Protfolio_Page> {
                         children: [
                           Image.asset(
                             "asset/images/SarthakJoshi_Resume_FlutterDeveloper_pages-to-jpg-0001.jpg",
-
                             fit: BoxFit.contain,
                           ),
-                          SizedBox(width:MediaQuery.of(context).size.width * 0.01,
-                        ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.01,
+                          ),
                           Image.asset(
                             "asset/images/SarthakJoshi_Resume_FlutterDeveloper_pages-to-jpg-0002.jpg",
-
                             fit: BoxFit.contain,
                           ),
                         ],
