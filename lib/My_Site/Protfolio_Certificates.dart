56ba79776b6c8a638f18b1f48540d6b0bb5ef567
Sarthak Joshi <141836239+sarthakdjoshi@users.noreply.github.com>
2024-10-19 11:25:23 +0530

    init commit

--- lib/My_Site/Protfolio_Certificates.dart
+++ lib/My_Site/Protfolio_Certificates.dart
@@ -19,7 +19,6 @@ class _ProtfolioCertificatesState extends State<ProtfolioCertificates> {
 
   // List of Google Drive URLs for certificates
   List<String> driveUrls = [
-    // Resume
     "https://drive.google.com/file/d/1-YO6w29FuIBUq_p_uQ5u4j3FR85nLn5-/view?usp=drive_link", // Experience Certificate
     "https://drive.google.com/file/d/1PSkeNG029EyH-QgC_hQ91PSnG9FGyFu2/view?usp=drive_link", // Letter of Recommendation
     "https://drive.google.com/file/d/103uwzKxorGottTwpzK_xrhgSbTrALUNw/view?usp=drive_link", // Power BI Certificate Tech tip 24
